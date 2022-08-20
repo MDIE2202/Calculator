bf12a40b5d48a391b7312e373632e094250d963c
MDIE2202 <marty.diender@gmail.com>
2022-08-20 18:25:53 +0200

    added a keyboard function to the calculator and some css hover effects. backspace and clear function also work properly now.

--- calc.js
+++ calc.js
@@ -9,25 +9,22 @@ clean = () =>{
 
 back = () =>{
     const disp = document.querySelector('.display');
-    disp.innerHTML = ''
+    disp.innerHTML = disp.innerHTML.slice(0, -1)
 }
 
 button = (sign) => {
     const disp = document.querySelector('.display');
-    if (sign == '+'){
+    allowed = ['1','2','3','4','5','6','7','8','9','0','+','-','/','*','(',')']
+    if (allowed.includes(sign)){
         disp.innerHTML += '' + sign
-    }else if( sign == '-'){
-        disp.innerHTML += '' + sign
-    }else if(sign == '*'){
-        disp.innerHTML += '' + sign
-    }else if(sign == '/'){
-        disp.innerHTML += '' + sign
-    }else if(sign == '='){
+    }else if(sign == '=' || sign == 'Enter'){
         disp.innerHTML = calculate(disp.innerHTML.replaceAll(/(\r\n|\n|\r)/gm,'').replaceAll(' ',''))
         console.log(disp.innerHTML)
+    }else if(sign == 'Backspace'){
+        back()
     }
     else{
-        disp.innerHTML += '' + sign
+        disp.innerHTML += ''
     }
 }
 
@@ -37,5 +34,7 @@ btns.forEach(btn => btn.addEventListener('click', function(){ button(this.innerH
 const clear = document.querySelector('.clear');
 clear.addEventListener('click', function(){clean()})
 
-const back = document.querySelector('.back');
-clear.addEventListener('click', function(){back()})
+const back_button = document.querySelector('.back');
+back_button.addEventListener('click', function(){back()})
+
+document.addEventListener('keydown', (event)=>{button(event.key)});
